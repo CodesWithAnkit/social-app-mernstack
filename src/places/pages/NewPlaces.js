5b5e22844bc61b010a47dd35cde72ae3b350f68a
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2019-12-24 15:44:09 +0530

    NewPlaces form

--- src/places/pages/NewPlaces.js
+++ src/places/pages/NewPlaces.js
@@ -50,8 +50,13 @@ const NewPlaces = () => {
     });
   }, []);
 
+  const placeSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
+
   return (
-    <form className="place-form">
+    <form className="place-form" onSubmit={placeSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -67,6 +72,14 @@ const NewPlaces = () => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid Description (at least 5 characters"
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid Address"
         onInput={inputHandler}
       />
       <Button type="submit" disabled={!formState.isValid}>
