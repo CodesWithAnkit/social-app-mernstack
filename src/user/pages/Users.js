c57fde9dbff67972961dcdad2b95910d1d3630d9
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2019-12-25 14:58:58 +0530

    Make a custom hook useForm

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import UsersList from '../components/UsersList';
+import UsersList from "../components/UsersList";
 
 const Users = () => {
   const USERS = [
     {
-      id: 'u1',
-      name: 'Max Schwarz',
+      id: "u1",
+      name: "Ankit Sharma",
       image:
-        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+        "https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/s960x960/37395355_999801846866030_2041513606373179392_o.jpg?_nc_cat=101&_nc_ohc=PSyz6lqP07cAQn0ebPXkHZ2IQN2SaBrzyatARWgNre79UoKXBhjNuUs0w&_nc_ht=scontent.fhyd6-1.fna&oh=e3c0e38e5114365bbe674263caea99f7&oe=5E6967D8",
       places: 3
     }
   ];
