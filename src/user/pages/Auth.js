2ca96c28657d69d95aba0977b2082e358b9228a6
codeswithankit <ank8804465648@gmail.com>
2020-04-14 21:45:56 +0530

    Connecting with backend and integirated signup features

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -3,10 +3,12 @@ import React, { useState, useContext } from 'react';
 import Card from '../../shared/components/UIElements/Card';
 import Button from '../../shared/components/FormElements/Button';
 import Input from '../../shared/components/FormElements/Input';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
-  VALIDATOR_REQUIRE
+  VALIDATOR_REQUIRE,
 } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
 import { AuthContext } from '../../shared/context/auth-context';
@@ -15,21 +17,50 @@ import './Auth.css';
 const Auth = () => {
   const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
   const [formState, inputHandler, setFormData] = useForm({
     email: {
       value: '',
-      isValid: false
+      isValid: false,
     },
     password: {
       value: '',
-      isValid: false
-    }
+      isValid: false,
+    },
   });
 
-  const authSubmitHandler = event => {
+  const authSubmitHandler = async (event) => {
     event.preventDefault();
-    console.log(formState.inputs);
-    auth.login();
+
+    if (isLoginMode) {
+    } else {
+      try {
+        setIsLoading(true);
+        const response = await fetch('http://localhost:5000/api/users/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: formState.inputs.name.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const responseData = await response.json();
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+        console.log(responseData);
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        setIsLoading(false);
+        setError(err.message || 'Something went wrong, please try again.');
+      }
+    }
   };
 
   const switchModeHandler = () => {
@@ -37,7 +68,7 @@ const Auth = () => {
       setFormData(
         {
           ...formState.inputs,
-          name: undefined
+          name: undefined,
         },
         formState.inputs.email.isValid && formState.inputs.password.isValid
       );
@@ -47,57 +78,63 @@ const Auth = () => {
           ...formState.inputs,
           name: {
             value: '',
-            isValid: false
-          }
+            isValid: false,
+          },
         },
         false
       );
     }
-    setIsLoginMode(prevMode => !prevMode);
+    setIsLoginMode((prevMode) => !prevMode);
   };
-
+  const errorHandler = () => {
+    setError(null);
+  };
   return (
-    <Card className="authentication">
-      <h2>Login Required</h2>
-      <hr />
-      <form onSubmit={authSubmitHandler}>
-        {!isLoginMode && (
+    <>
+      <ErrorModal error={error} onClear={errorHandler} />
+      <Card className="authentication">
+        {isLoading && <LoadingSpinner asOverlay />}
+        <h2>Login Required</h2>
+        <hr />
+        <form onSubmit={authSubmitHandler}>
+          {!isLoginMode && (
+            <Input
+              element="input"
+              id="name"
+              type="text"
+              label="Your Name"
+              errorText="Please Input a Name"
+              onInput={inputHandler}
+              validators={[VALIDATOR_REQUIRE]}
+            />
+          )}
           <Input
             element="input"
-            id="name"
-            type="text"
-            label="Your Name"
-            errorText="Please Input a Name"
+            type="email"
+            label="E-mail"
+            id="email"
+            validators={[VALIDATOR_EMAIL()]}
+            errorText="Please Enter a Valid EMail Address"
             onInput={inputHandler}
-            validators={[VALIDATOR_REQUIRE]}
           />
-        )}
-        <Input
-          element="input"
-          type="email"
-          label="E-mail"
-          id="email"
-          validators={[VALIDATOR_EMAIL()]}
-          errorText="Please Enter a Valid EMail Address"
-          onInput={inputHandler}
-        />
-        <Input
-          element="input"
-          type="password"
-          label="Password"
-          id="password"
-          validators={[VALIDATOR_MINLENGTH(5)]}
-          errorText="Please Enter a Valid Password at least 5"
-          onInput={inputHandler}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-          {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+          <Input
+            element="input"
+            type="password"
+            label="Password"
+            id="password"
+            validators={[VALIDATOR_MINLENGTH(5)]}
+            errorText="Please Enter a Valid Password at least 5"
+            onInput={inputHandler}
+          />
+          <Button type="submit" disabled={!formState.isValid}>
+            {isLoginMode ? 'LOGIN' : 'SIGNUP'}
+          </Button>
+        </form>
+        <Button inverse onClick={switchModeHandler}>
+          SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
         </Button>
-      </form>
-      <Button inverse onClick={switchModeHandler}>
-        SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
-      </Button>
-    </Card>
+      </Card>
+    </>
   );
 };
 export default Auth;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import UsersList from '../components/UsersList';
+import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
   const USERS = [
     {
-      id: 'u1',
-      name: 'Ankit Sharma',
+      id: "u1",
+      name: "Ankit Sharma",
       image:
-        'https://scontent.fhyd6-1.fna.fbcdn.net/v/t1.0-9/s960x960/37395355_999801846866030_2041513606373179392_o.jpg?_nc_cat=101&_nc_ohc=PSyz6lqP07cAQn0ebPXkHZ2IQN2SaBrzyatARWgNre79UoKXBhjNuUs0w&_nc_ht=scontent.fhyd6-1.fna&oh=e3c0e38e5114365bbe674263caea99f7&oe=5E6967D8',
-      places: 3
-    }
+        "https://res.cloudinary.com/practicaldev/image/fetch/s--b7PKk4zJ--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/212362/4960dfa6-8124-4c98-947b-b69265df3def.jpeg",
+      places: 3,
+    },
   ];
 
   return <UsersList items={USERS} />;
